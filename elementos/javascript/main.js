247c530d40f7793e77b0c732c429f27bb5c25e08
ls1w <michelle_j.b@hotmail.com>
2023-11-16 09:04:42 -0300

    elementos 70% ok

---
+++ elementos/javascript/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const botaoSequencia = document.getElementById('botao-sequencia');
+
+
+function gerarSequencia () {
+    const quantidade = document.getElementById('quantidade');
+    const resultado = document.getElementById('resultado');
+
+    resultado.textContent = 3
+}
+
+
+
+
+botaoSequencia.addEventListener('click' , gerarSequencia);
