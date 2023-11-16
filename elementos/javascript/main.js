0e0954d281e24799bf99c126e935e726bf412200
ls1w <michelle_j.b@hotmail.com>
2023-11-16 11:05:46 -0300

    Sequencia, Impares, Pares e Multiplos de 5 ok

--- elementos/javascript/main.js
+++ elementos/javascript/main.js
@@ -1,16 +1,53 @@
 'use strict'
 
 const botaoSequencia = document.getElementById('botao-sequencia');
-
+const botaoImpar = document.getElementById('botao-impar');
+const botaoPar = document.getElementById('botao-par');
+const botaoMultiplos = document.getElementById('botao-multiplos');
+const quantidade = document.getElementById('quantidade');
 
 function gerarSequencia () {
-    const quantidade = document.getElementById('quantidade');
-    const resultado = document.getElementById('resultado');
+    // const quantidade = document.getElementById('quantidade');
+    const resultadoSequencia = document.getElementById('resultado-sequencia');
 
-    resultado.textContent = 3
-}
+    resultadoSequencia.textContent = "";
+    for (let contador = 0; contador <= quantidade.value; contador++){
+        resultadoSequencia.textContent += contador + " ";
+    };
+};
+
+function gerarNumerosImpares () {
+    const resultadoImpar = document.getElementById('resultado-impar');
+
+    resultadoImpar.textContent = "";
+    for (let contador = 1; contador <= quantidade.value; contador+=2){
+        resultadoImpar.textContent += contador + " ";
+    };
+};
 
 
+function gerarNumerosPares () {
+    const resultadoPar = document.getElementById('resultado-par');
 
+    resultadoPar.textContent = "";
+    for (let contador = 0; contador <= quantidade.value; contador+=2) {
+        resultadoPar.textContent += contador + " ";
+        
+    };
+};
+
+function gerarNumerosMultiplos () {
+    const resultadoMultiplos = document.getElementById('resultado-multiplos');
+
+    resultadoMultiplos.textContent = "";
+    for (let contador = 1; contador <= quantidade.value; contador++) {
+        if (contador % 5 == 0) {
+            resultadoMultiplos.textContent += contador + " ";
+        };
+    };
+};
 
 botaoSequencia.addEventListener('click' , gerarSequencia);
+botaoImpar.addEventListener('click' , gerarNumerosImpares);
+botaoPar.addEventListener('click' , gerarNumerosPares);
+botaoMultiplos.addEventListener('click' , gerarNumerosMultiplos);
