b7008cd08a88757a4538271df032e1882a745f09
ls1w <michelle_j.b@hotmail.com>
2023-12-04 10:34:43 -0300

    add apidog ok

---
+++ apidog/javascript/main.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const btnpesquisar = document.getElementById('pesquisar');
+const textRaca = document.getElementById('raca');
+
+function limparImagens() {
+    const container = document.getElementById('galeria');
+
+    container.textContent = '';
+};
+
+function criarImagem(urlImagem) {
+    const container = document.getElementById('galeria');
+    const imagem = document.createElement('img');
+
+    imagem.src = urlImagem;
+    container.appendChild(imagem);
+};
+
+async function carregarGalerias() {
+    const raca = document.getElementById('raca').value.toLowerCase();
+    const url = `https://dog.ceo/api/breed/${raca}/images`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    limparImagens();
+    data.message.forEach(criarImagem);
+};
+
+function capturarEnter(event) {
+    if ( event.inputType == 'insertLineBreak') {
+        carregarGalerias();
+    };
+};
+
+btnpesquisar.addEventListener('click', carregarGalerias);
+
+textRaca.addEventListener('beforeinput' , capturarEnter);
